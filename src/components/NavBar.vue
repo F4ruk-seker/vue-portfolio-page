95aac5264403329d975924cb9e9cc0ecb039d7dc
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-12-19 12:13:44 +0300

    add nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,8 +3,35 @@
 </script>
 
 <template>
-  <nav class="position-fixed text-light z-2">
-    pars
+  <nav
+      class="navbar navbar-expand-lg w-100 position-fixed text-light z-2 shadow"
+      style="background: linear-gradient(90deg, rgba(2,0,36,.01) 0%, rgba(2,171,228,.1) 50%, rgba(0,212,255,.01) 100%);backdrop-filter:blur(3px);-webkit-backdrop-filter:blur(3px);"
+  >
+    <div class="container">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link fw-bold text-light active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold text-light" aria-current="About" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold text-light" aria-current="Experiences" href="#experiences">Experiences</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold text-light" aria-current="Contact" href="#contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold text-light" aria-current="Game" href="#Game">Game</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold text-light" aria-current="Blog" href="#blog">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold text-light" aria-current="Game" href="#Game">Game</a>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
