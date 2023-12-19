91b1da3cf60fec47d4044e01ca6bd62df64e26f7
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-12-19 14:18:24 +0300

    change footer

--- src/components/MyFoter.vue
+++ src/components/MyFoter.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-<footer>
-  <article>
-    foter
+<footer
+    class="bg-dark d-flex "
+    style="background: linear-gradient(90deg, rgba(2,0,36,.2) 0%, rgba(2,171,228,.5) 50%, rgba(0,212,255,.2) 100%)"
+>
+  <article class="container mx-auto my-5 justify-content-center text-center row">
+    <p class="fw-bold text-light m-0 p-0">
+      Faruk Şeker - sync app - cookies and other rights reserved - <a class="link-info" href="mailto:info@farukseker.com.tr">
+        info@farukseker.com.tr
+      </a>
+    </p>
   </article>
 </footer>
 </template>
