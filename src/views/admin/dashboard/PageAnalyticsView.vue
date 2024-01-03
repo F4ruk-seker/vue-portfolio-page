60cb4eef7d7c7cf572fd81b9a8a39c920be6134a
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-03 13:43:46 +0300

    change page analytics

--- src/views/admin/dashboard/PageAnalyticsView.vue
+++ src/views/admin/dashboard/PageAnalyticsView.vue
@@ -56,7 +56,7 @@ async function fetch_page() {
         <button @click="show_views = true" class="btn btn-outline-primary border border-dark me-1">Grap</button>
         <button @click="show_views = false" class="btn btn-light ms-1">User Agents</button>
       </div>
-      <div v-if="!show_views">
+      <div v-if="show_views">
         <div v-for="(mhv,mhk, index) in page.monthly_plot" v-bind:key="index">
           {{mhk}}
           <div class="row">
@@ -68,12 +68,20 @@ async function fetch_page() {
           </div>
         </div>
       </div>
-      <div v-else>
+      <div v-else class="mb-3">
+        <ul class="list-unstyled d-flex w-100 m-1 border-bottom">
+          <li class="w-100 text-start">#</li>
+          <li class="w-100 text-center" style="width: min-content">visit_time</li>
+          <li class="w-100 text-center">method</li>
+          <li class="w-100 text-center">Country</li>
+          <li class="w-100 text-center">OHAC</li>
+          <li><i class="fa-brands fa-nfc-directional"></i></li>
+        </ul>
         <div v-for="(view, index) in page.view" v-bind:key="view.id" class="w-100">
           <ul class="list-unstyled d-flex w-100 m-1 border-bottom" style="cursor: pointer" @click="view.toggle = !view.toggle">
             <li class="w-100 text-start">{{index+1}}</li>
             <li class="w-100 text-center" style="width: min-content">{{ view.visit_time }}</li>
-            <li class="w-100 text-center">{{ view.method ? view.method:'unknown' }}</li>
+            <li class="w-100 text-center">{{ view.request_type ? view.request_type:'unknown' }}</li>
             <li class="w-100 text-center">{{ view.ip_data.Country ? view.ip_data.Country:'unknown' }}</li>
             <li class="w-100 text-center">{{ view.reload_count_in_a_clock ? view.reload_count_in_a_clock:'NaN' }}</li>
             <li>
@@ -81,19 +89,40 @@ async function fetch_page() {
               <span v-else><i class="fa-solid fa-angle-down"></i></span>
             </li>
           </ul>
-          <div v-if="view.toggle" class="d-flex">
-            <div class="w-100 border-end">
-              <div class="row m-0 p-0">
-                <div class="col-2">IS User</div><div class="col-10"><i class="fa-solid fa-check"></i></div>
-                <div class="col-2">Country</div><div class="col-10">{{view.ip_data.Country ? view.ip_data.Country:'unknown'}}</div>
-                <div class="col-2">City</div><div class="col-10">{{view.ip_data.City ? view.ip_data.City:'unknown'}}</div>
-                <div class="col-2">City</div><div class="col-10">{{view.ip_data['Time Zone'] ? view.ip_data['Time Zone']:'unknown'}}</div>
+          <div v-if="view.toggle" class="border border-primary">
+            <div class="d-flex border-bottom">
+              <div class="w-100">
+                <strong>Request</strong>
+                <div class="row m-0 p-0">
+                  <div class="col-3">IS User</div><div class="col-9">
+                  <span v-if="view.is_i_am"><i class="fa-solid fa-check"></i></span>
+                  <span v-else><i class="fa-solid fa-xmark"></i></span>
+                </div>
+                  <div class="col-3">Country</div><div class="col-9">{{view.ip_data.Country ? view.ip_data.Country:'unknown'}}</div>
+                  <div class="col-3">City</div><div class="col-9">{{view.ip_data.City ? view.ip_data.City:'unknown'}}</div>
+                  <div class="col-3">State / Region</div><div class="col-9">{{view.ip_data['State / Region'] ? view.ip_data['State / Region']:'unknown'}}</div>
+                  <div class="col-3">Time Zone</div><div class="col-9">{{view.ip_data['Time Zone'] ? view.ip_data['Time Zone']:'unknown'}}</div>
+                  <div class="col-3">Lat / Long</div><div class="col-9">{{view.ip_data['Lat / Long'] ? view.ip_data['Lat / Long']:'unknown'}}</div>
+                  <div class="col-3">Continent</div><div class="col-9">{{view.ip_data.Continent ? view.ip_data.Continent:'unknown'}}</div>
+                </div>
+              </div>
+              <div class="w-100 border-start">
+                <strong>Request Agent</strong>
+                <div class="row m-0 p-0">
+                  <div class="col-3">Host</div><div class="col-9"> {{ view.ip_data.host }}</div>
+                  <div class="col-3">Provider</div><div class="col-9"> {{ view.ip_data.provider }}</div>
+                  <div class="col-3">Metro Code</div><div class="col-9">{{view.ip_data['Metro Code'] ? view.ip_data['Metro Code']:'unknown'}}</div>
+                  <div class="col-3">Postal code</div><div class="col-9">{{view.ip_data['Postal code'] ? view.ip_data['Postal code']:'unknown'}}</div>
+                  <div class="col-3">User Agent</div><div class="col-9">{{view.user_agent ? view.user_agent:'unknown'}}</div>
+                </div>
               </div>
             </div>
-            <div class="w-100 border-end">
+            <div>
+              <strong>Request Detail</strong>
               <div class="row m-0 p-0">
-                <div class="col-2"></div>
-                <div class="col"></div>
+                <div class="col-3">TYPE</div><div class="col-9">{{ view.request_type ? view.request_type:'unknown' }}</div>
+                <div class="col-3">query_string</div><div class="col-9">{{ view.query_string ? view.query_string:'unknown'}}</div>
+                <div class="col-3">data:</div><div class="col-9">{{ view.request_data ? view.request_data:'unknown'}}</div>
               </div>
             </div>
           </div>
