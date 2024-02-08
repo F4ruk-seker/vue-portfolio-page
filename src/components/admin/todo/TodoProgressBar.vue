04e539e020b65649296e08eab7b60ab8d1486e71
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-02-08 04:47:40 +0300

    add progress bar color

--- src/components/admin/todo/TodoProgressBar.vue
+++ src/components/admin/todo/TodoProgressBar.vue
@@ -1,12 +1,23 @@
 <script>
 export default {
     name: 'TodoProgress',
-    props: ['percentage']
+    props: ['percentage'],
+    data: ()=>{return{
+      color: 'secondary'
+    }},
+    watch: {
+      percentage(new_percentage){
+        if (!(new_percentage > 25)){this.color = 'danger'}
+        else if (!(new_percentage > 50)){this.color = 'warning'}
+        else if (!(new_percentage > 75)){this.color = 'info'}
+        else {this.color = 'success'}
+      }
+    }
 }
 </script>
 
 <template>
 <div class="progress">
-  <div class="progress-bar bg-success" role="progressbar" :style="'width: ' + percentage + '%'" :aria-valuenow="percentage" aria-valuemin="0" aria-valuemax="100"></div>
+  <div :class="'progress-bar bg-'+ color" role="progressbar" :style="'width: ' + percentage + '%'" :aria-valuenow="percentage" aria-valuemin="0" aria-valuemax="100"></div>
 </div>
 </template>
