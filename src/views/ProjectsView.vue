15df9893230a5ea0d285e822d24e17e0419a0042
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-02-08 17:30:43 +0300

    project filter

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -2,7 +2,8 @@
 import NavbarStable from "@/components/NavbarStable.vue";
 import ScrolTop from "@/components/ScrolTop.vue";
 import axios from "axios";
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
+
 // import router from "@/router";
 // import TagHeadSearch from "@/components/TagHeadSearch.vue";
 
@@ -16,10 +17,35 @@ onMounted(()=>{fetch_project_filters();fetch_projects()})
 const projects = ref([])
 const filter_keys = ref({})
 const toggle_card_view = ref(false)
+
+// const selected_tags = ref([])
+
 const usersLanguage = window.navigator.language
+const search_text = ref('')
+
 console.log(usersLanguage);
-async function fetch_projects(){
-  await axios.get('content/all/project').then((response)=>{
+
+watch(search_text, async (new_search_text)=>{
+  console.log('join w');
+  await fetch_projects(new_search_text)
+})
+
+
+function set_filters(){
+  const selecteds = []
+  filter_keys.value.forEach((tag_c)=>{
+      tag_c.tags.forEach((tag)=>{
+        if (tag.selected){selecteds.push(tag.id); console.log('catch-',tag.id)}
+        else {selecteds.pop(tag.id)}
+      })
+    })
+  console.log(selecteds)
+}
+
+
+// ?search=test
+async function fetch_projects(search=''){
+  await axios.get(`content/all/?content_type=project&search=${search}`).then((response)=>{
     // response.data.forEach((project)=>{project.created = new Date(project.created)});projects.value=response.data
       console.log(response.data);
       response.data.forEach((project) => {
@@ -60,10 +86,9 @@ async function fetch_projects(){
 async function fetch_project_filters(){
   await axios.get('content/type/project').then(response=> {
     filter_keys.value = response.data.sub_tags;
+    
   }) 
 }
-
-
 
 
 </script>
@@ -77,10 +102,10 @@ async function fetch_project_filters(){
     <div v-for="(tags, index) in filter_keys" v-bind:key="index">
       <strong style="font-size: 24px;">{{tags.name}}</strong>
       <ul class="list-unstyled tree-list" >
-        <li class="d-flex" v-for="(tag, index) in tags.tags" v-bind:key="index">
+        <li class="d-flex" v-for="(tag, index) in tags.tags" v-bind:key="index" >
           <hr class="my-auto fw-bold" style="width: 10px; height:2px">
           <label class="my-auto">
-            <input class="mx-1" type="checkbox">
+            <input class="mx-1" type="checkbox" v-model="tag.selected" @click="set_filters"> <!--need filter toggle -->
             {{ tag.name }}
           </label>
         </li>
@@ -107,7 +132,7 @@ async function fetch_project_filters(){
           <span class="my-auto" style="width: max-content;">{{projects.length}} Result</span>
         </div>
         <div class="w-100 mx-2 position-relative">
-          <input class="form-control rounded-5" style="padding-right: 48px;">
+          <input class="form-control rounded-5" style="padding-right: 48px;" type="text" v-model="search_text">
           <div class="d-flex position-absolute btn-search">
             <button class="btn btn-danger rounded-circle h-100 w-100 ps-2 pt-1">
               <i class="fa-solid fa-magnifying-glass justify-content-center m-auto"></i>
