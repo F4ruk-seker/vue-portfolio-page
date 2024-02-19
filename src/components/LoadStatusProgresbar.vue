da4c726a1024caacf7a631e9d1be79d8627f73c6
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-02-19 17:08:58 +0300

    change position head porgressbar

--- src/components/LoadStatusProgresbar.vue
+++ src/components/LoadStatusProgresbar.vue
@@ -1,19 +1,15 @@
 
 
 <template>
-<section>
-    <article class="position-relative top-0">
-        <div v-if="$store.getters.getProgressShow" class="progress rounded-0 position-sticky top-0 start-0 z-3">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                role="progressbar"
-                :style="'width: ' + $store.getters.getProgressStatus +'%'"
-                :aria-valuenow="$store.getters.getProgressStatus"
-                aria-valuemin="0"
-                aria-valuemax="100">
-            </div>
-        </div>
-    </article>
-</section>
+<div v-if="$store.getters.getProgressShow" class="progress rounded-0 position-sticky top-0 start-0 z-3">
+        <div class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+        role="progressbar"
+        :style="'width: ' + $store.getters.getProgressStatus +'%'"
+        :aria-valuenow="$store.getters.getProgressStatus"
+        aria-valuemin="0"
+        aria-valuemax="100">
+    </div>
+</div>
 </template>
 
 <script setup>
