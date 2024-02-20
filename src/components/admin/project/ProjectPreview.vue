7748fa7813e5b37578f3353afb441437737f7cb9
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-02-20 04:51:56 +0300

    Change project edit list view

--- src/components/admin/project/ProjectPreview.vue
+++ src/components/admin/project/ProjectPreview.vue
@@ -7,15 +7,16 @@ export default {
 </script>
 
 <template>
-    <router-link class="text-decoration-none" :to="{name:'admin-project', params:{slug:project.slug}}">
-        <div class="btn btn-light p-1 w-100 my-1 d-flex">
+    <div class="" >
+        <router-link class="text-decoration-none h-100" style="min-height: 75px;"  :to="{name:'admin-project', params:{slug:project.slug}}">
+        <div class="btn btn-light p-1 w-100 my-1 d-flex py-2" style="height: max-content;">
             <div class="d-flex" style="min-width: 50px;">
                 <strong class="justify-content-center m-auto">
                     {{ row + 1 }} / {{ project.id }}
                 </strong>
             </div>
-            <div class="bg-secondary rounded h-100" style="min-width: 100px; min-height: 75px;">
-                ...
+            <div class="bg-secondary rounded h-100 ratio ratio-16x9" style="max-width: 100px; height: 75px; min-height: 75px;">
+                <img :src="project.seo_image_url" alt="project banner" style="object-fit: cover;">
             </div>
             <div class="ms-2 my-auto">
                 <strong class="p-1">{{ project.title }}</strong>
@@ -23,6 +24,7 @@ export default {
             </div>
         </div>
     </router-link>
+    </div>
 </template>
 
 <style>
